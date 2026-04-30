35b845ee4ffcb2d28e7ccb1b4f6824af9366d5ed
nicksit12 <117672206+nicksit12@users.noreply.github.com>
2026-04-30 00:37:06 -0400

    Checkpoint 4

--- src/main/java/com/example/weather_app/controller/WeatherController.java
+++ src/main/java/com/example/weather_app/controller/WeatherController.java
@@ -3,10 +3,22 @@ package com.example.weather_app.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping; 
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.weather_app.client.OpenMeteoClient;
+
+
 
 @RestController 
 public class WeatherController {
+
+    private final OpenMeteoClient openMeteoClient;
+
+    public WeatherController(OpenMeteoClient openMeteoClient){
+        this.openMeteoClient = openMeteoClient; 
+    }
+
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of("message", "Weather app is running");
@@ -16,4 +28,11 @@ public class WeatherController {
     public Map<String, String> health(){ 
         return Map.of("status", "ok"); 
     }
+
+    @GetMapping("/forecast/raw")
+    public String getRawForecast( @RequestParam double longitude, @RequestParam double latitude) 
+    {
+        return openMeteoClient.getForecastJson(latitude, longitude);
+    } 
+    
 }
